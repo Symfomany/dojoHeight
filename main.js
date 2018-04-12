82252750d6975732d0d80530c04ddd09cca2e7a9
Symfomany <zuzu38080@gmail.com>
2018-04-12 12:37:24 +0200

    ok

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-function hamming(phraseOne, phraseTwo) {}
+function hamming(phraseOne, phraseTwo) {
+  let phraseOneClear = phraseOne.replace(/\s/gi, "");
+  let phraseTwoClear = phraseTwo.replace(/\s/gi, "");
+  //console.log(phraseOneClear, phraseTwoClear);
 
-hamming("Je suis une phrase", "Je suis une autre phrase");
+  if (phraseOneClear.length !== phraseTwoClear.length) {
+    return -1;
+  }
+  let splited = phraseOneClear.split("");
+  let splited2 = phraseTwoClear.split("");
+  //console.log(splited, splited2);
+
+  let compteur = 0;
+
+  if (phraseOneClear === phraseTwoClear) {
+    return 0;
+  }
+
+  for (let [index, value] of splited.entries()) {
+    //console.log(value, index);
+    //console.log(splited2[index]);
+    if (splited[index] !== splited2[index]) {
+      compteur++;
+      //console.log(compteur);
+    }
+  }
+  return compteur;
+}
+
+let resultat = hamming("Jee", "Je e");
+
+console.log(resultat);
